be99ab494bf7102c333590e2f426a4569b86f89c
Murat Ertik <dmuratertik1@gmail.com>
2025-01-27 11:00:41 -0800

    User role enum has been created.

---
+++ src/main/java/com/project/ecommerce/domain/USER_ROLE.java
@@ -0,0 +1,9 @@
+package com.project.ecommerce.domain;
+
+public enum USER_ROLE {
+    //sabit değerleri bir grup altında toplamak için kullanılan bir veri yapısıdır
+    ROLE_ADMIN,
+    ROLE_CUSTOMER,
+    ROLE_SELLER
+
+}
