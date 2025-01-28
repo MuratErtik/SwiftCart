da3fcb74038f0630dfa4e211a30f21f370328141
Murat Ertik <dmuratertik1@gmail.com>
2025-01-28 06:16:46 -0800

    Category,Review,Seller entities have been completed.

--- src/main/java/com/project/ecommerce/entities/Category.java
+++ src/main/java/com/project/ecommerce/entities/Category.java
@@ -1,5 +1,54 @@
 package com.project.ecommerce.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @NotNull
+    @Column(unique = true)
+    private String categoryId;
+
+    @ManyToOne
+    private Category parentCategory;
+
+    @NotNull
+    private Integer level;
+
+    /*
+     * @NotNull: Bu anotasyon, genellikle doğrulama
+     * (validation) amaçlı kullanılır. Yani, uygulama çalışırken bir nesnenin bu
+     * alanda null olmaması gerektiğini belirtir. Bu anotasyon, genellikle kullanıcı
+     * girişini kontrol etmek, veritabanına kaydetmeden önce geçerliliğini sağlamak
+     * için kullanılır.
+     * ******************************************************************************* *
+     * @Column(nullable = false): Bu anotasyon, JPA (Java Persistence API) ve
+     * Hibernate gibi ORM (Object-Relational Mapping) araçları ile veritabanı
+     * tabloları üzerinde çalışırken kullanılır. nullable = false, veritabanındaki
+     * ilgili sütunun null değer kabul etmeyeceğini belirtir. Yani, veritabanı
+     * şemasını oluştururken bu sütunun null olamayacağı zorunlu hale gelir.
+     */
 
 }
--- src/main/java/com/project/ecommerce/entities/Seller.java
+++ src/main/java/com/project/ecommerce/entities/Seller.java
@@ -1,5 +1,63 @@
 package com.project.ecommerce.entities;
 
+import com.project.ecommerce.domain.AccountStatus;
+import com.project.ecommerce.domain.USER_ROLE;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
+    private String sellerName;
+
+    private String mobile;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    @Embedded
+    private BusinessDetails businessDetails = new BusinessDetails();
+
+    /*
+     * bir sınıfın, başka bir sınıfın bir parçası olarak bir başka sınıfa gömülmesini sağlar. 
+     * Bu, bir sınıfın başka bir sınıfa yerleştirilmesi anlamına gelir ve genellikle değer türleri (value types) için kullanılır.
+     */
+    @Embedded
+    private BankDetails bankDetails = new BankDetails();
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address pickupAddress = new Address();
+
+    private USER_ROLE role=USER_ROLE.ROLE_SELLER;
+
+    private boolean isEmailVerified = false;
+
+    private AccountStatus accountStatus = AccountStatus.PENDING_VERIFICATION;
+
+
 
 }
