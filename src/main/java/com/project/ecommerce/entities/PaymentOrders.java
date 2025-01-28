24bf37dd64ac8fa33e63aea77176f8b87b347793
Murat Ertik <dmuratertik1@gmail.com>
2025-01-28 09:07:50 -0800

    new entities have been added v2.

---
+++ src/main/java/com/project/ecommerce/entities/PaymentOrders.java
@@ -0,0 +1,49 @@
+package com.project.ecommerce.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.project.ecommerce.domain.PaymentMethod;
+import com.project.ecommerce.domain.PaymentOrderStatus;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class PaymentOrders {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Long amount;
+
+    private PaymentOrderStatus status = PaymentOrderStatus.PENDING ;
+
+    private PaymentMethod paymentMethod;
+
+    private String paymentLinkid;
+
+    @ManyToOne
+    private User user;
+
+    @OneToMany
+    private Set<Order> orders = new HashSet<>();
+
+
+
+
+}
