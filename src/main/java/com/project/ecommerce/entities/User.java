d501a42559b81e0fadf1746faed74385310b45aa
Murat Ertik <dmuratertik1@gmail.com>
2025-01-28 01:25:18 -0800

    Annotations have been updated.

--- src/main/java/com/project/ecommerce/entities/User.java
+++ src/main/java/com/project/ecommerce/entities/User.java
@@ -3,6 +3,7 @@ package com.project.ecommerce.entities;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.project.ecommerce.domain.USER_ROLE;
 
@@ -11,19 +12,32 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+
     /*
      * Yazma (Deserialization): Bu alanın JSON'dan nesneye dönüşümü sırasında kullanılmasına izin verilir.
        Okuma (Serialization): Bu alanın JSON oluşturulurken dahil edilmesine izin verilmez.
      */
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     private String email;
@@ -34,9 +48,15 @@ public class User {
 
     private USER_ROLE role = USER_ROLE.ROLE_CUSTOMER; 
 
+    @OneToMany
     private Set<Address> addresses = new HashSet<>();
 
+    @ManyToMany
+    @JsonIgnore
     private Set<Coupon> usedCoupons = new HashSet<>();
+    /*
+     * JSON serileştirme ve JSON deserialization (JSON'dan Java nesnesine dönüşüm) işlemlerinde yok sayılmasını sağlar.
+     */
 
 
 }
