d5309dcbf349c08dbfce71e18d276453562290bf
Murat Ertik <dmuratertik1@gmail.com>
2025-01-31 00:31:29 -0800

    Cart and User repository has been created for signup

---
+++ src/main/java/com/project/ecommerce/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package com.project.ecommerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.ecommerce.entities.Cart;
+
+public interface CartRepository extends JpaRepository<Cart,Long> {
+
+}
