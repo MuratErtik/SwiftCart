276ebeb63d25ea91d65d61e6f18c8a2497b6fb7f
Murat Ertik <dmuratertik1@gmail.com>
2025-01-31 00:33:10 -0800

    Auth Service has been created.

--- src/main/java/com/project/ecommerce/services/AuthService.java
+++ src/main/java/com/project/ecommerce/services/AuthService.java
@@ -1,5 +1,64 @@
 package com.project.ecommerce.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.project.ecommerce.config.JwtProvider;
+import com.project.ecommerce.domain.USER_ROLE;
+import com.project.ecommerce.entities.Cart;
+import com.project.ecommerce.entities.User;
+import com.project.ecommerce.repository.CartRepository;
+import com.project.ecommerce.repository.UserRepository;
+import com.project.ecommerce.responses.SignupRequest;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final CartRepository cartRepository;
+    private final JwtProvider jwtProvider;
+
+    public String createUser(SignupRequest request) {
+        User user = userRepository.findByEmail(request.getEmail());
+
+        if (user == null) {
+            User newUser = new User();
+
+            newUser.setEmail(request.getEmail());
+
+            newUser.setFullName(request.getFullName());
+
+            newUser.setRole(USER_ROLE.ROLE_CUSTOMER);
+
+            newUser.setMobile("5827328392");
+
+            newUser.setPassword(passwordEncoder.encode(request.getOtp()));
+
+            user = userRepository.save(newUser);
+
+            Cart cart = new Cart();
+            cart.setUser(user);
+            cartRepository.save(cart);
+
+        }
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(USER_ROLE.ROLE_CUSTOMER.toString())); //user.getRole().toString())
+        Authentication authentication = new UsernamePasswordAuthenticationToken(request.getEmail(), null, authorities);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return jwtProvider.generateToken(authentication);
+    }
 
 }
