11902bf7ec3b56c1405c996125fcdbd66ba27268
Murat Ertik <dmuratertik1@gmail.com>
2025-02-03 08:56:05 -0800

    filter query has been completed.

--- src/main/java/com/project/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/project/ecommerce/repository/ProductRepository.java
@@ -4,16 +4,31 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.project.ecommerce.entities.Product;
 
-public interface ProductRepository extends JpaRepository<Product,Long> , JpaSpecificationExecutor<Product> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     /*
-     *  Filtreleme (kategori, renk, beden, fiyat aralığı gibi dinamik parametreler)
-        Arama kriterlerine göre özelleştirilmiş SQL sorguları oluşturma
-        Büyük veri kümelerinde esnek ve optimize sorgular yapma
+     * Filtreleme (kategori, renk, beden, fiyat aralığı gibi dinamik parametreler)
+     * Arama kriterlerine göre özelleştirilmiş SQL sorguları oluşturma
+     * Büyük veri kümelerinde esnek ve optimize sorgular yapma
      */
     List<Product> findBySellerId(Long sellerId);
+
+    // JPQL (Java Persistence Query Language)
+    // Buradaki p, Product entity'sini temsil eder.
+    // Kullanıcının bir sorgu (query) girmediği durumda, tüm ürünleri getir.
+    // lower(p.title) → Harf duyarlılığını kaldırır (BÜYÜK/küçük harf farkını
+    // önler).
+    // title içinde query geçiyorsa bu ürünü getir.
+
+    
+    @Query("SELECT p FROM Product p WHERE (:query IS NULL OR lower(p.title) LIKE lower(concat('%',:query,'%')) " +
+       "OR lower(p.category.name) LIKE lower(concat('%',:query,'%')))")
+    List<Product> searchProducts(@Param("query") String query);
+
     
 
 }
