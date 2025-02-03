0bfdc3b586dd15266f43ff968fa66721b7d858ee
Murat Ertik <dmuratertik1@gmail.com>
2025-02-03 14:48:31 -0800

    Cart Service has been created.

--- src/main/java/com/project/ecommerce/services/CartService.java
+++ src/main/java/com/project/ecommerce/services/CartService.java
@@ -1,5 +1,19 @@
 package com.project.ecommerce.services;
 
+import org.springframework.stereotype.Service;
+
+import com.project.ecommerce.repository.CartItemRepository;
+import com.project.ecommerce.repository.CartRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class CartService {
 
+    private final CartRepository cartRepository;
+
+    private final CartItemRepository cartItemRepository;
+
+    
 }
