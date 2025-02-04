11ec3c0e8baed30f8e8e7b85989e70ff955f2d2b
Murat Ertik <dmuratertik1@gmail.com>
2025-02-04 11:54:52 -0800

    PaymentOrderException has been completed.

--- src/main/java/com/project/ecommerce/exceptions/GlobalException.java
+++ src/main/java/com/project/ecommerce/exceptions/GlobalException.java
@@ -8,68 +8,68 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
-
 //Spring Boot'ta hata yönetimi yapmak için kullanılır.
 //Böylece, her Controller içinde ayrı ayrı hata yönetimi yazmak zorunda kalmayız.
-
 
 @ControllerAdvice
 public class GlobalException {
 
     // bir sellerexcep olursa bu method devreye girer.
     @ExceptionHandler(SellerException.class)
-    public ResponseEntity<ErrorDetail> sellerExceptionHandler(SellerException se , WebRequest webRequest){
+    public ResponseEntity<ErrorDetail> sellerExceptionHandler(SellerException se, WebRequest webRequest) {
 
         ErrorDetail errorDetail = new ErrorDetail();
         errorDetail.setError(se.getMessage());
         errorDetail.setDetails(webRequest.getDescription(false));
         errorDetail.setTimestamp(LocalDateTime.now());
 
-        return new ResponseEntity<>(errorDetail,HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorDetail, HttpStatus.BAD_REQUEST);
 
     }
 
     @ExceptionHandler(ProductException.class)
-    public ResponseEntity<ErrorDetail> productExceptionHandler(ProductException pe, WebRequest webRequest){
+    public ResponseEntity<ErrorDetail> productExceptionHandler(ProductException pe, WebRequest webRequest) {
 
         ErrorDetail errorDetail = new ErrorDetail();
         errorDetail.setError(pe.getMessage());
         errorDetail.setDetails(webRequest.getDescription(false));
         errorDetail.setTimestamp(LocalDateTime.now());
 
-        return new ResponseEntity<>(errorDetail,HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorDetail, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(CartException.class)
-    public ResponseEntity<ErrorDetail> cartExceptionHandler(CartException ce, WebRequest webRequest){
+    public ResponseEntity<ErrorDetail> cartExceptionHandler(CartException ce, WebRequest webRequest) {
         ErrorDetail errorDetail = new ErrorDetail();
         errorDetail.setError(ce.getMessage());
         errorDetail.setDetails(webRequest.getDescription(false));
         errorDetail.setTimestamp(LocalDateTime.now());
 
-        return new ResponseEntity<>(errorDetail,HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorDetail, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(OrderException.class)
-    public ResponseEntity<ErrorDetail> orderExceptionHandler(OrderException oe , WebRequest webRequest){
+    public ResponseEntity<ErrorDetail> orderExceptionHandler(OrderException oe, WebRequest webRequest) {
 
         ErrorDetail errorDetail = new ErrorDetail();
         errorDetail.setError(oe.getMessage());
         errorDetail.setDetails(webRequest.getDescription(false));
         errorDetail.setTimestamp(LocalDateTime.now());
 
-        return new ResponseEntity<>(errorDetail,HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorDetail, HttpStatus.BAD_REQUEST);
 
     }
+
+    @ExceptionHandler(PaymentOrderException.class)
+    public ResponseEntity<ErrorDetail> paymentOrderException(PaymentOrderException poe, WebRequest webRequest) {
+
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setError(poe.getMessage());
+        errorDetail.setDetails(webRequest.getDescription(false));
+        errorDetail.setTimestamp(LocalDateTime.now());
+
+        return new ResponseEntity<>(errorDetail, HttpStatus.BAD_REQUEST);
+
+    }
+
 }
-
-
-
-
-
-
-
-
-
-
-
---
+++ src/main/java/com/project/ecommerce/exceptions/PaymentOrderException.java
@@ -0,0 +1,8 @@
+package com.project.ecommerce.exceptions;
+
+public class PaymentOrderException extends Exception {
+
+    public PaymentOrderException(String message){
+        super(message);
+    }
+}
