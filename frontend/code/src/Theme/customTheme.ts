7971abfe137c5386efcb2b76c60adebcfa79a188
Murat Ertik <dmuratertik1@gmail.com>
2025-02-07 04:20:15 -0800

    has been created for priority colors

---
+++ frontend/code/src/Theme/customTheme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+const customeTheme = createTheme({
+    palette: {
+        mode: "light",
+        primary: {
+            main: "#009FFD"
+        },
+        secondary: {
+            main: "#B0E0E6"
+        }
+    }
+})
+
+export default customeTheme;
