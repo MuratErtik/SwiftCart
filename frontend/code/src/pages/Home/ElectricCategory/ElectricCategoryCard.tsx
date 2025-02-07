febc257110594db837f0d921e0d513182d90641d
Murat Ertik <dmuratertik1@gmail.com>
2025-02-07 06:46:55 -0800

    ElectricCategory has been completed.

---
+++ frontend/code/src/pages/Home/ElectricCategory/ElectricCategoryCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ElectricCategoryCard = () => {
+    return (
+        <div>
+            <img className="object-contain h-10" src="https://images.pexels.com/photos/812264/pexels-photo-812264.jpeg?auto=compress&cs=tinysrgb&w=1200" alt="" />
+            <h2 className="font-semibold text-sm">Laptop</h2>
+        </div>
+    )
+}
+
+export default ElectricCategoryCard
