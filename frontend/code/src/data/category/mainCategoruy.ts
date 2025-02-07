a93b6021b22e04c49ab76bafa5a60bb0d278f85a
Murat Ertik <dmuratertik1@gmail.com>
2025-02-07 10:27:16 -0800

    all categories have been indicated.

---
+++ frontend/code/src/data/category/mainCategoruy.ts
@@ -0,0 +1,153 @@
+export const mainCategory = [
+    {
+        name: "Men",
+        categoryid: "men",
+        level: 1,
+        levelTwoCategory: [
+            {
+                "name": "Topwere",
+                "categoryId": "men_topwear",
+                "parentCategoryId": "men",
+                "level": 2
+            }, 
+            {
+                "name": "Bottomwere",
+                "categoryId": "men_bottomwear",
+                "parentCategoryId": "men",
+                "level": 2
+            },
+            {
+                "name": "Footwear",
+                "categoryId": "men_footwear",
+                "parentCategoryId": "men",
+                "level": 2
+            },
+            {
+                "name": "Accessories",
+                "categoryId": "men_accessories",
+                "parentCategoryId": "men",
+                "level": 2
+            },
+            {
+                "name": "Ethnic Wear",
+                "categoryId": "men_ethnicwear",
+                "parentCategoryId": "men",
+                "level": 2
+            }]
+
+
+    },
+    {
+        name: "Women",
+        categoryid: "women",
+        level: 1,
+        levelTwoCategory: [
+            {
+                "name": "Topwear",
+                "categoryId": "women_topwear",
+                "parentCategoryId": "women",
+                "level": 2
+            }, 
+            {
+                "name": "Bottomwear",
+                "categoryId": "women_bottomwear",
+                "parentCategoryId": "women",
+                "level": 2
+            },
+            {
+                "name": "Footwear",
+                "categoryId": "women_footwear",
+                "parentCategoryId": "women",
+                "level": 2
+            },
+            {
+                "name": "Accessories",
+                "categoryId": "women_accessories",
+                "parentCategoryId": "women",
+                "level": 2
+            },
+            {
+                "name": "Ethnic Wear",
+                "categoryId": "women_ethnicwear",
+                "parentCategoryId": "women",
+                "level": 2
+            }]
+
+
+    },
+    {
+        name:"Home & Furniture",
+        categoryId:"home_furniture",
+        level : 1,
+        levelTwoCategory: [
+            {
+                "name": "Living Room Furniture",
+                "categoryId": "home_furniture_livingroom",
+                "parentCategoryId": "home_furniture",
+                "level": 2
+            }, 
+            {
+                "name": "Bedroom Furniture",
+                "categoryId": "home_furniture_bedroom",
+                "parentCategoryId": "home_furniture",
+                "level": 2
+            },
+            {
+                "name": "Dining Room Furniture",
+                "categoryId": "home_furniture_diningroom",
+                "parentCategoryId": "home_furniture",
+                "level": 2
+            },
+            {
+                "name": "Office Furniture",
+                "categoryId": "home_furniture_office",
+                "parentCategoryId": "home_furniture",
+                "level": 2
+            },
+            {
+                "name": "Outdoor Furniture",
+                "categoryId": "home_furniture_outdoor",
+                "parentCategoryId": "home_furniture",
+                "level": 2
+            }
+        ]
+
+    },
+    {
+        name:"Electronics",
+        categoryId:"electronics",
+        level:1,
+        levelTwoCategory:[
+            {
+                "name": "Mobile Phones",
+                "categoryId": "electronic_mobilephones",
+                "parentCategoryId": "electronic",
+                "level": 2
+            }, 
+            {
+                "name": "Laptops",
+                "categoryId": "electronic_laptops",
+                "parentCategoryId": "electronic",
+                "level": 2
+            },
+            {
+                "name": "Televisions",
+                "categoryId": "electronic_televisions",
+                "parentCategoryId": "electronic",
+                "level": 2
+            },
+            {
+                "name": "Cameras",
+                "categoryId": "electronic_cameras",
+                "parentCategoryId": "electronic",
+                "level": 2
+            },
+            {
+                "name": "Smartwatches",
+                "categoryId": "electronic_smartwatches",
+                "parentCategoryId": "electronic",
+                "level": 2
+            }    
+        ]
+    }
+];
