1b909ea5f7ba2fda031f3cf2c07763bb0540e915
Murat Ertik <dmuratertik1@gmail.com>
2025-02-08 10:26:00 -0800

    new datas have been added.

---
+++ frontend/code/src/data/color.ts
@@ -0,0 +1,23 @@
+export const colors=[
+    {"name": "Navy Blue", "hex": "#000080"},
+    {"name": "Maroon", "hex": "#800000"},
+    {"name": "Peach", "hex": "#FFDAB9"},
+    {"name": "White", "hex": "#FFFFFF"},
+    {"name": "Grey", "hex": "#808080"},
+    {"name": "Teal", "hex": "#008080"},
+    {"name": "Turquoise Blue", "hex": "#00CED1"},
+    {"name": "Mustard", "hex": "#FFDB58"},
+    {"name": "Beige", "hex": "#F5F5DC"},
+    {"name": "Cream", "hex": "#FFFDD0"},
+    {"name": "Sea Green", "hex": "#2E8B57"},
+    {"name": "Orange", "hex": "#FFA500"},
+    {"name": "Brown", "hex": "#A52A2A"},
+    {"name": "Off White", "hex": "#FAF0E6"},
+    {"name": "Burgundy", "hex": "#800020"},
+    {"name": "Lavender", "hex": "#E6E6FA"},
+    {"name": "Sky Blue", "hex": "#87CEEB"},
+    {"name": "Coral", "hex": "#FF7F50"},
+    {"name": "Dark Green", "hex": "#006400"},
+    {"name": "Gold", "hex": "#FFD700"}
+  ]
+  
