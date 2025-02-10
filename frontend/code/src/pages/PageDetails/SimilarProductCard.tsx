ec5f06a334019f0a14f4575bffe2d1497031f560
Murat Ertik <dmuratertik1@gmail.com>
2025-02-10 07:07:15 -0800

    has been created.

--- frontend/code/src/pages/PageDetails/SimilarProductCard.tsx
+++ frontend/code/src/pages/PageDetails/SimilarProductCard.tsx
@@ -2,7 +2,36 @@ import React from "react";
 
 const SimilarProductCard = () => {
     return (
-        <div>fdkkdlfkdfldfkl</div>
+        <div>
+            <div className='group px-4 relative'>
+                <div className="card">
+                    <img className="card-media object-top" src="https://media.istockphoto.com/id/1354020636/tr/fotoğraf/black-t-shirt-mockup-front-used-as-design-template-tee-shirt-blank-isolated-on-white.jpg?s=2048x2048&w=is&k=20&c=afjaZvVS8-kbBsbj7oMGUTF0Ww6ujL1mMPF2skyct1Q=" alt=""
+                    />
+
+
+                </div>
+                <div className='details pt-3 space-y-1 group-hover-effect rounded-md'>
+                    <div className="name">
+                        <h1>H&M</h1>
+                        <p>White Tshirt</p>
+                    </div>
+                    <div className='price flex items-center gap-3'>
+                        <span className="font-sans •text-gray-800">
+                            400
+                        </span>
+                        <span className="thin-line-trhough text-gray-400">
+                            <s>999</s>
+                        </span>
+                        <span className="text-primary-color font-semibold">
+                            %60
+                        </span>
+
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
