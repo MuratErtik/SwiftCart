3b531695d365c8c7ed6df191a5f688a4acca86ce
Murat Ertik <dmuratertik1@gmail.com>
2025-02-10 05:27:57 -0800

    These classes have been created for products section.

---
+++ frontend/code/src/pages/PageDetails/SimilarProductCard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SimilarProductCard = () => {
+    return (
+        <div>fdkkdlfkdfldfkl</div>
+    )
+}
+
+export default SimilarProductCard
---
+++ frontend/code/src/pages/Review/ReviewCard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ReviewCard = () => {
+    return (
+        <div>fdkkdlfkdfldfkl</div>
+    )
+}
+
+export default ReviewCard
