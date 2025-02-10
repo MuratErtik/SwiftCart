8146abf86e125b4602ddecac16d374d09475a45e
Murat Ertik <dmuratertik1@gmail.com>
2025-02-10 07:07:40 -0800

    part of review has been created.

--- frontend/code/src/pages/Review/Review.tsx
+++ frontend/code/src/pages/Review/Review.tsx
@@ -1,9 +1,38 @@
 import React from "react";
+import StarIcon from '@mui/icons-material/Star';
+import { Divider } from "@mui/material";
+import ReviewCard from "./ReviewCard.tsx";
+
 
 const Review = () => {
     return (
-        <div>fdkkdlfkdfldfkl</div>
+        <div className='p-5 lg:px-20 flex flex-col lg:flex-row gap-20'>
+            <section className='w-full md:w-1/2 1g:w-[30%] space-y-2'>
+                <img src="https://images.pexels.com/photos/9558786/pexels-photo-9558786.jpeg?auto=compress&cs=tinysrgb&w=1200" alt="" />
+                <div>
+                    <div>
+                        <p className='font-bold text-xl'>Clothing</p>
+                        <p className='text-lg text-gray-600'>Men's White Shirt</p>
+                    </div>
+                    <div>
+                        <div className='price flex items-center gap-3 mt-5 text-2xl'>
+                            <span className="font-sans text-gray-800">250</span>
+                            <span className="line-through text-gray-400">449</span>
+                            <span className="text-primary-color font-semibold">%25</span>
+
+                        </div>
+                        
+                    </div>
+
+                </div>
+            </section>
+            <section>
+                {[1,1,1,1,1,1].map((item) => <div className='spyЗ'><Divider/> <ReviewCard/></div>)}
+            </section>
+        </div>
     )
 }
 
 export default Review
+
+
--- frontend/code/src/pages/Review/ReviewCard.tsx
+++ frontend/code/src/pages/Review/ReviewCard.tsx
@@ -1,8 +1,42 @@
+import { Delete } from "@mui/icons-material";
+import { Avatar, Box, Grid, Grid2, IconButton, Rating } from "@mui/material";
+import { red } from "@mui/material/colors";
 import React from "react";
 
 const ReviewCard = () => {
     return (
-        <div>fdkkdlfkdfldfkl</div>
+        <div className='flex justify-between'>
+            <Grid2 container spacing={8} >
+                <Grid2 size={{ xs: 1 }}>
+                    <Box>
+                        <Avatar className='text-white' sx={{ width: 56, height: 56, bgcolor: "#9155FD" }}>
+                            M
+                        </Avatar>
+                    </Box>
+                </Grid2>
+                <Grid2 size={{ xs: 9 }}>
+                    <div className='space-y-2'>
+                        <div>
+                            <p className='font-semibold text-lg'>MuratErtik</p>
+                            <p className='opacity-70' >2025-02-10: 16:07</p>
+                        </div>
+                    </div>
+                    <Rating readOnly value={4} precision={1} />
+                    <p>valuable for spend money.</p>
+                    <div>
+                        <img className='w-24 h-24 object-cover' src="https://images.pexels.com/photos/9558786/pexels-photo-9558786.jpeg?auto=compress&cs=tinysrgb&w=1200" alt="" />
+                    </div>
+
+                </Grid2>
+
+            </Grid2>
+            <div>
+                <IconButton>
+                    <Delete sx={{ color: red[700] }} />
+                </IconButton>
+            </div>
+
+        </div>
     )
 }
 
