c2495c595b1fbcd6a5b17af704db17a8950f418c
Murat Ertik <dmuratertik1@gmail.com>
2025-02-14 10:59:52 -0800

    has been created for seller.

---
+++ frontend/code/src/seller/page/Payment/Transaction.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Transaction = () =>{
+    return(
+        <div>dsdsdsds</div>
+    )
+}
+
+export default Transaction
---
+++ frontend/code/src/seller/page/Products/AddProducts.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AddProducts = () =>{
+    return(
+        <div></div>
+    )
+}
+
+export default AddProducts
