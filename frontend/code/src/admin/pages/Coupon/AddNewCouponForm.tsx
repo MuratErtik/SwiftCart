64bf76b193fafefb2118ea929bb41fac29c7ce2e
Murat Ertik <dmuratertik1@gmail.com>
2025-02-15 11:31:28 -0800

    these files has been created.

---
+++ frontend/code/src/admin/pages/Coupon/AddNewCouponForm.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+const AddNewCouponForm = () => {
+    return(
+        <div>
+           sd
+        </div>
+    )
+}
+
+export default AddNewCouponForm
